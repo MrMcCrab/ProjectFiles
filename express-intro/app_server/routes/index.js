9ff2ac5804b0641879adeb53e08fab0cb13378ce
MrMcCrab <>
2018-02-01 11:00:39 +0200

    Update app to MVC architecture

--- express-intro/app_server/routes/index.js
+++ express-intro/app_server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let ctrlMain = require('../controllers/main');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Juuso' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
